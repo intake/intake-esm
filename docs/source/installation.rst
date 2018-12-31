4257bff79329965a92a711721f1839f71718f908
Anderson Banihirwe <axbanihirwe@ualr.edu>
2018-12-31 09:57:42 -0700

    add boilerplate

---
+++ docs/source/installation.rst
@@ -0,0 +1,8 @@
+============
+Installation
+============
+
+At the command line either via conda or pip::
+
+    $ conda install intake-cesmle
+    $ pip install intake-cesmle
---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+import versioneer
+from os.path import exists
+
+readme = open("README.rst").read() if exists("README.rst") else ""
+
+
+
+setup(
+    name='intake-cesmle',
+    description='Intake-cesmle provides a plug for reading CESM Large Ensemble dataasets using intake',
+    long_description=readme,
+    maintainer='Anderson Banihirwe',
+    maintainer_email='abanihi@ucar.edu',
+    url='https://github.com/NCAR/intake-cesmle',
+    packages=[
+        'intake-cesmle',
+    ],
+    package_dir={'intake-cesmle': 'intake-cesmle'},
+    include_package_data=True,
+    install_requires=[
+    ],
+    license='Apache-2.0 license',
+    zip_safe=False,
+    keywords='intake-cesmle',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+)
