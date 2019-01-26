ef8fa7a09a6d3405d5818aca1be4c7314ac31535
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-01-25 18:05:32 -0700

    Add cesm catalog

---
+++ intake_cesm/config.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+""" The configuration utility script. This script is used
+to pass CESM database/files to intake_cesm DataSources.
+"""
+
+from __future__ import absolute_import, print_function
+
+import os
+
+collection_dir = os.path.join(os.path.dirname(__file__), "collections")
+
+collections = {
+    "cesm_dple": os.path.join(collection_dir, "cesm_dple.csv"),
+    "cesm2_runs": os.path.join(collection_dir, "cesm2_runs.csv"),
+    "cesm1_le": os.path.join(collection_dir, "cesm1_le.csv"),
+}
