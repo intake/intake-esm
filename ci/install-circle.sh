b05f293771eee625edc4099972157ef48dc550f5
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-01-30 10:52:34 -0700

    Fix package installation

--- ci/install-circle.sh
+++ ci/install-circle.sh
@@ -11,5 +11,6 @@ conda env create -f ci/environment-dev-${PYTHON}.yml --name=${ENV_NAME} --quiet
 conda env list
 source activate ${ENV_NAME}
 pip install pip --upgrade
-pip install --no-deps --quiet -e .
+# pip install --no-deps --quiet -e .
+python setup.py develop
 conda list -n ${ENV_NAME}
