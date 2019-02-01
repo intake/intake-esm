ee3b9ebb739e38ed48483161f6c4c7770bb2361c
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-02-01 14:37:31 -0700

    Remove pyproject.yml as it was causing some issues

--- ci/install-circle.sh
+++ ci/install-circle.sh
@@ -11,6 +11,5 @@ conda env create -f ci/environment-dev-${PYTHON}.yml --name=${ENV_NAME} --quiet
 conda env list
 source activate ${ENV_NAME}
 pip install pip --upgrade
-# pip install --no-deps --quiet -e .
-python setup.py develop
+pip install --no-deps --quiet -e .
 conda list -n ${ENV_NAME}
