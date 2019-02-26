053f700de13a156d1f414cae542e64cc3c1d8cf8
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-02-26 07:35:52 -0700

    Fix isort issues

--- intake_esm/__init__.py
+++ intake_esm/__init__.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 """ Top-level module for intake_esm. """
 from .core import ESMMetadataStoreCatalog
-from .config import set_options, get_options, SETTINGS, SOURCES
-
 from ._version import get_versions
+from .config import set_options, get_options, SETTINGS, SOURCES
 
 __version__ = get_versions()["version"]
 del get_versions
