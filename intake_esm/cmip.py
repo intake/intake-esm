60ceb2cc3d5c1adabab1294e245a8d8fadd25578
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-03-01 10:53:37 -0700

    Add implementation for CMIPCollection with tests

--- intake_esm/cmip.py
+++ intake_esm/cmip.py
@@ -1,18 +1,149 @@
+import logging
 import os
+import re
+import shutil
+from pathlib import Path
 
+import dask.dataframe as dd
 import pandas as pd
+from dask import delayed
 from intake_xarray.netcdf import NetCDFSource
 
 from .common import Collection, StorageResource
 from .config import INTAKE_ESM_CONFIG_FILE, SETTINGS
 
+logger = logging.getLogger(__name__)
+logger.setLevel(level=logging.WARNING)
+
 
 class CMIPCollection(Collection):
-    def __init__(self):
-        raise NotImplementedError
+    def __init__(self, collection_spec):
+        super(CMIPCollection, self).__init__(collection_spec)
+        self.df = pd.DataFrame()
+        self.root_dir = self.collection_spec['data_sources']['root_dir']['urlpath']
+
+    def _validate(self):
+        for req_col in ['realm', 'frequency', 'ensemble', 'experiment', 'file_fullpath']:
+            if req_col not in self.columns:
+                raise ValueError(
+                    f"Missing required column: {req_col} for {self.collection_spec['collection_type']} in {INTAKE_ESM_CONFIG_FILE}"
+                )
 
     def build(self):
-        raise NotImplementedError
+        """ Build collection and return a pandas Dataframe"""
+        self._validate()
+        if not os.path.exists(self.root_dir):
+            raise NotADirectoryError(f'{self.root_dir} does not exist')
+
+        dirs = _parse_dirs(self.root_dir)
+        dfs = [_parse_directory(directory, self.columns) for directory in dirs]
+        df = dd.from_delayed(dfs).compute()
+
+        # NOTE: This is not a robust solution in case the root dir does not match the pattern
+        vYYYYMMDD = r'v\d{4}\d{2}\d{2}'
+        vN = r'v\d{1}'
+        v = re.compile('|'.join([vYYYYMMDD, vN]))  # Combine both regex into one
+        df['version'] = df['files_dirname'].str.findall(v)
+        df['version'] = df['version'].apply(lambda x: x[0] if x else 'v0')
+        sorted_df = (
+            df.sort_values('version')
+            .drop_duplicates(subset='file_basename', keep='last')
+            .reset_index(drop=True)
+        )
+        self.df = sorted_df.copy()
+        logger.warning(self.df.info())
+        logger.warning(f"Persisting {self.collection_spec['name']} at : {self.collection_db_file}")
+        self.df.to_csv(self.collection_db_file, index=True)
+        return self.df
+
+
+def _parse_dirs(root_dir):
+    institution_dirs = [
+        os.path.join(root_dir, activity, institution)
+        for activity in os.listdir(root_dir)
+        for institution in os.listdir(os.path.join(root_dir, activity))
+        if os.path.isdir(os.path.join(root_dir, activity, institution))
+    ]
+
+    model_dirs = [
+        os.path.join(institution_dir, model)
+        for institution_dir in institution_dirs
+        for model in os.listdir(institution_dir)
+        if os.path.isdir(os.path.join(institution_dir, model))
+    ]
+
+    experiment_dirs = [
+        os.path.join(model_dir, exp)
+        for model_dir in model_dirs
+        for exp in os.listdir(model_dir)
+        if os.path.isdir(os.path.join(model_dir, exp))
+    ]
+
+    freq_dirs = [
+        os.path.join(experiment_dir, freq)
+        for experiment_dir in experiment_dirs
+        for freq in os.listdir(experiment_dir)
+        if os.path.isdir(os.path.join(experiment_dir, freq))
+    ]
+
+    realm_dirs = [
+        os.path.join(freq_dir, realm)
+        for freq_dir in freq_dirs
+        for realm in os.listdir(freq_dir)
+        if os.path.isdir(os.path.join(freq_dir, realm))
+    ]
+
+    return realm_dirs
+
+
+def _get_entry(directory):
+    dir_split = directory.split('/')
+    entry = {}
+    entry['realm'] = dir_split[-1]
+    entry['frequency'] = dir_split[-2]
+    entry['experiment'] = dir_split[-3]
+    entry['model'] = dir_split[-4]
+    entry['institution'] = dir_split[-5]
+    return entry
+
+
+@delayed
+def _parse_directory(directory, columns):
+    exclude = set(['files', 'latests'])  # directories to exclude
+
+    df = pd.DataFrame(columns=columns)
+
+    entry = _get_entry(directory)
+
+    for root, dirs, files in os.walk(directory):
+        # print(root)
+        dirs[:] = [d for d in dirs if d not in exclude]
+        if not files:
+            continue
+        sfiles = sorted([f for f in files if os.path.splitext(f)[1] == '.nc'])
+        if not sfiles:
+            continue
+
+        fs = []
+        for f in sfiles:
+            try:
+                f_split = f.split('_')
+                entry['variable'] = f_split[0]
+                entry['ensemble'] = f_split[-2]
+                entry['files_dirname'] = root
+                entry['file_basename'] = f
+                entry['file_fullpath'] = os.path.join(root, f)
+                fs.append(entry)
+            except BaseException:
+                continue
+        if fs:
+            temp_df = pd.DataFrame(fs)
+
+        else:
+            temp_df = pd.DataFrame()
+            temp_df.columns = df.columns
+        df = pd.concat([temp_df, df], ignore_index=True, sort=False)
+    return df
 
 
 class CMIPSource(NetCDFSource):
--- tests/test_cmip.py
+++ tests/test_cmip.py
@@ -1,11 +1,13 @@
+import os
+
+import intake
+import pandas as pd
 import pytest
 
-from intake_esm.cmip import CMIPCollection, CMIPSource
+here = os.path.abspath(os.path.dirname(__file__))
 
 
-def test_collection_constructor():
-    pass
-
-
-def test_source_constructor():
-    pass
+def test_build_collection():
+    collection_input_file = os.path.join(here, 'cmip_collection_input_test.yml')
+    col = intake.open_esm_metadatastore(collection_input_file=collection_input_file)
+    assert isinstance(col.df, pd.DataFrame)
