2a16d80a6625e6cc30d6ae5a5e80d4de99b54be0
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-03-04 13:23:20 -0700

    Fix isort formatting issue

--- intake_esm/__init__.py
+++ intake_esm/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """ Top-level module for intake_esm. """
-from ._version import get_versions
 from . import config
+from ._version import get_versions
 from .core import ESMMetadataStoreCatalog
 
 __version__ = get_versions()['version']
