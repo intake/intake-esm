47dc9a0947945ff846fecdbf5bc7e1da294697cd
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-04-10 15:17:33 -0600

    Remove unnecessary code for the time being

--- intake_esm/mpige.py
+++ intake_esm/mpige.py
@@ -2,15 +2,71 @@ import logging
 import os
 import re
 
+import numpy as np
 import pandas as pd
+import xarray as xr
+from tqdm.autonotebook import tqdm
 
+from . import aggregate, config
 from .cesm import CESMCollection
+from .common import BaseSource, Collection, StorageResource, get_subset
 
 logger = logging.getLogger(__name__)
 logger.setLevel(level=logging.WARNING)
 
 
 class MPIGECollection(CESMCollection):
+    def assemble_file_list(self, experiment, experiment_attrs, component_attrs, ensembles):
+        df_files = {}
+        for location in experiment_attrs['locations']:
+            res_key = ':'.join([location['name'], location['loc_type'], location['urlpath']])
+            if res_key not in df_files:
+                logger.warning(f'Getting file listing : {res_key}')
+
+                if 'exclude_dirs' not in location:
+                    location['exclude_dirs'] = []
+
+                resource = StorageResource(
+                    urlpath=location['urlpath'],
+                    loc_type=location['loc_type'],
+                    exclude_dirs=location['exclude_dirs'],
+                )
+
+                df_files[res_key] = self._assemble_collection_df_files(
+                    resource_key=res_key,
+                    resource_type=location['loc_type'],
+                    direct_access=location['direct_access'],
+                    filelist=resource.filelist,
+                )
+
+        # # Loop over ensemble members
+        # for ensemble, ensemble_attrs in enumerate(ensembles):
+        #     input_attrs_base = {'experiment': experiment}
+
+        #     # Get attributes from ensemble_attrs
+        #      #case = ensemble_attrs['case']
+
+        #     if 'ensemble' not in ensemble_attrs:
+        #         input_attrs_base.update({'ensemble': ensemble})
+
+        #     if 'sequence_order' not in ensemble_attrs:
+        #         input_attrs_base.update({'sequence_order': 0})
+
+        #     if 'has_ocean_bgc' not in ensemble_attrs:
+        #         input_attrs_base.update({'has_ocean_bgc': False})
+
+        #     if 'ctrl_branch_year' not in ensemble_attrs:
+        #         input_attrs_base.update({'ctrl_branch_year': np.datetime64('NaT')})
+
+        self.df = pd.concat(df_files.values())
+        # Reorder columns
+        # self.df = self.df[self.columns]
+
+        # Remove duplicates
+        self.df = self.df.drop_duplicates(
+            subset=['resource', 'file_fullpath'], keep='last'
+        ).reset_index(drop=True)
+
     def _assemble_collection_df_files(self, resource_key, resource_type, direct_access, filelist):
 
         entries = {
