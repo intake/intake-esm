1320757286619cb973791fbe31cf8d919b872fff
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-04-11 19:19:16 -0600

    Add mpige test for search method

--- tests/test_mpige.py
+++ tests/test_mpige.py
@@ -17,3 +17,12 @@ def test_build_collection():
             collection_input_definition=collection_input_definition, overwrite_existing=True
         )
         assert isinstance(col.df, pd.DataFrame)
+
+
+def test_search():
+    with config.set({'database-directory': './tests/test_collections'}):
+        c = intake.open_esm_metadatastore(collection_name='mpige_test')
+        cat = c.search(component='mpiom', stream='monitoring_ym')
+
+        assert isinstance(cat.query_results, pd.DataFrame)
+        assert not cat.query_results.empty
