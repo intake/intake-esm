3d50931583073c124ec341caaa4bde4b582453ca
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-06-14 12:12:46 -0600

    Add hsi test

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -1,10 +1,16 @@
 import os
+import re
+import socket
 
 import pytest
 
 from intake_esm.storage import StorageResource
 
 here = os.path.abspath(os.path.dirname(__file__))
+
+regex = re.compile(r'cheyenne|casper')
+hostname = socket.gethostname()
+match = regex.search(hostname)
 
 
 def test_storage_input_file():
@@ -19,3 +25,18 @@ def test_storage_input_file():
     files = SR._list_files_input_file()
     assert isinstance(files, list)
     assert len(files) == 5
+
+
+@pytest.mark.skipif(
+    not match, reason='does not run outside of Cheyenne login nodes or Casper nodes'
+)
+def test_storage_hsi():
+    urlpath = '/CCSM/csm/CESM-CAM5-BGC-LE'
+    loc_type = 'hsi'
+    exclude_dirs = []
+    file_extension = '.nc'
+    SR = StorageResource(urlpath, loc_type, exclude_dirs, file_extension)
+
+    files = SR._list_files_hsi()
+    assert isinstance(files, list)
+    assert len(files) != 0
