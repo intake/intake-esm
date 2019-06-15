6b0c807928b60f7f7ab5d34190b0dd0bccc585b3
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-06-14 19:55:47 -0600

    Remove print function

--- tests/test_definition_utils.py
+++ tests/test_definition_utils.py
@@ -8,7 +8,6 @@ filepaths = list(FILE_ALIAS_DICT.keys())
 @pytest.mark.parametrize('filepath', filepaths)
 def test_open_collection_def_locally(filepath):
     """Opens all files listed in file_alias_dict."""
-    print(filepath)
     d = load_collection_definition(filepath)
     assert isinstance(d, dict)
     assert len(d) > 0
