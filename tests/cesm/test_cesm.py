add4dbde155b65031074ef796624175337b936d4
mclong <mclong@ucar.edu>
2019-06-16 11:13:31 -0600

    fixed dsets access bug

--- tests/cesm/test_cesm.py
+++ tests/cesm/test_cesm.py
@@ -56,14 +56,7 @@ def test_to_xarray_cesm(chunks, expected_chunks):
 @pytest.mark.skipif(
     not match, reason='does not run outside of Cheyenne login nodes or Casper nodes'
 )
-@pytest.mark.parametrize(
-    'chunks, expected_chunks',
-    [
-        ({'time': 100, 'nlat': 2, 'nlon': 2}, (1, 100, 2, 2)),
-        ({'time': 200, 'nlat': 1, 'nlon': 1}, (1, 200, 1, 1)),
-    ],
-)
-def test_to_xarray_cesm_hsi(chunks, expected_chunks):
+def test_to_xarray_cesm_hsi():
     data_cache_dir = f'{TMPDIR}/intake-esm-tests/transferred-data'
     with config.set(
         {'database-directory': './tests/test_collections', 'data-cache-directory': data_cache_dir}
@@ -72,10 +65,10 @@ def test_to_xarray_cesm_hsi(chunks, expected_chunks):
             here, '../ensure-file-hsi-transfer-collection-input.yml'
         )
         col = intake.open_esm_metadatastore(
-            collection_input_definition=collection_input_definition, overwrite_existing=True
+            collection_input_definition=collection_input_definition, overwrite_existing=False
         )
 
         cat = col.search(variable=['SST'])
-        dset = cat.to_xarray(chunks=chunks)
-        ds = dset['pop.h.ocn']
-        assert ds['SST'].data.chunksize == expected_chunks
+        dset = cat.to_xarray(chunks={'time': 365})
+        _, ds = dset.popitem()
+        assert isinstance(ds['SST'], xr.DataArray)
