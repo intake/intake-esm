669c2b5bcffd1050feadca5520137459c34c9cb2
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-08-05 20:48:48 -0600

    Define intake.drivers entrypoints

--- setup.py
+++ setup.py
@@ -43,10 +43,10 @@ setup(
     install_requires=install_requires,
     license='Apache 2.0',
     zip_safe=False,
-    entry_points="""
-    [console_scripts]
-    intake-esm-builder=intake_esm.cli:main
-    """,
+    entry_points={
+        'console_scripts': ['intake-esm-builder = intake_esm.cli:main'],
+        'intake.drivers': ['esm_metadatastore = intake_esm.core:ESMMetadataStoreCatalog'],
+    },
     keywords='intake-esm',
     use_scm_version={'version_scheme': 'post-release', 'local_scheme': 'dirty-tag'},
     setup_requires=['setuptools_scm', 'setuptools>=30.3.0'],
