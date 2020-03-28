cec5cfb9222f806bf62462131428fd9313af5c37
Anderson Banihirwe <axbanihirwe@ualr.edu>
2020-03-28 16:38:12 -0600

    Test exceptions

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,28 @@
+import sys
+from unittest import mock
+
 import pandas as pd
 import pytest
 import requests
 
-from intake_esm.utils import _fetch_and_parse_json, _fetch_catalog, _get_dask_client
+from intake_esm.utils import _fetch_and_parse_json, _fetch_catalog, _get_dask_client, _is_valid_url
+
+
+def test_invalid_url():
+    url = 'http://www.example.com/file[/].html'
+    assert not _is_valid_url(url)
+
+
+def test_is_valid_url_not_found():
+    url = 'https://raw.githubusercontent.com/NCAR/esm-collection-spec/master/collection-spec/examples/sample-glade-cmip6-netcdf-collection.json'
+    with mock.patch('requests.get') as mock_request:
+        mock_request.return_value.status_code = 404
+        assert not _is_valid_url(url)
+
+
+def test_is_valid_url_exception():
+    url = 55555
+    assert not _is_valid_url(url)
 
 
 def test_fetch_and_parse_json_url():
@@ -10,6 +30,12 @@ def test_fetch_and_parse_json_url():
     data, path = _fetch_and_parse_json(url)
     assert path == url
     assert requests.get(url).json() == data
+
+
+def test_fetch_and_parse_json_error():
+    url = 'https://raw.githubusercontent.com/NCAR/esm-collection-spec/master/README.md'
+    with pytest.raises(Exception):
+        _fetch_and_parse_json(url)
 
 
 def test_fetch_and_parse_json_local(sample_cmip6):
@@ -31,9 +57,7 @@ def test_catalog_url_construction_from_relative_url():
 
 
 def test_get_dask_client():
-    from unittest import mock
     from distributed import Client
-    import sys
 
     with Client() as client:
         c, _is_client_local = _get_dask_client()
