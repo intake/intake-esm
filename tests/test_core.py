1a2b4ccbba3f7c65020a5f9841f645bddd37ad96
Anderson Banihirwe <axbanihirwe@ualr.edu>
2020-06-30 04:32:05 -0600

    Support regular expression objects in `search()` (#236)

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,4 +1,5 @@
 import os
+import re
 from tempfile import TemporaryDirectory
 
 import intake
@@ -391,6 +392,27 @@ params = [
             {'A': 'NCAR', 'B': 'CESM', 'C': 'control', 'D': 'O2'},
         ],
     ),
+    (
+        {'D': [re.compile(r'^O2$'), 'NO2'], 'B': ['CESM', 'BAR']},
+        None,
+        [
+            {'A': 'NCAR', 'B': 'CESM', 'C': 'hist', 'D': 'O2'},
+            {'A': 'CSIRO', 'B': 'BAR', 'C': 'control', 'D': 'O2'},
+            {'A': 'NCAR', 'B': 'CESM', 'C': 'control', 'D': 'O2'},
+        ],
+    ),
+    ({'C': ['hist'], 'D': ['TA']}, None, [{'A': 'NCAR', 'B': 'WACM', 'C': 'hist', 'D': 'TA'}],),
+    (
+        {
+            'C': [re.compile('hist.*', flags=re.IGNORECASE)],
+            'D': [re.compile('TA.*', flags=re.IGNORECASE)],
+        },
+        None,
+        [
+            {'A': 'NCAR', 'B': 'WACM', 'C': 'hist', 'D': 'TA'},
+            {'A': 'NASA', 'B': 'foo', 'C': 'HiSt', 'D': 'tAs'},
+        ],
+    ),
 ]
 
 
@@ -398,10 +420,10 @@ params = [
 def test_get_subset(query, require_all_on, expected):
     df = pd.DataFrame(
         {
-            'A': ['NCAR', 'IPSL', 'IPSL', 'CSIRO', 'IPSL', 'NCAR', 'NOAA', 'NCAR'],
-            'B': ['CESM', 'FOO', 'FOO', 'BAR', 'FOO', 'CESM', 'GCM', 'WACM'],
-            'C': ['hist', 'control', 'hist', 'control', 'hist', 'control', 'hist', 'hist'],
-            'D': ['O2', 'O2', 'O2', 'O2', 'NO2', 'O2', 'O2', 'TA'],
+            'A': ['NCAR', 'IPSL', 'IPSL', 'CSIRO', 'IPSL', 'NCAR', 'NOAA', 'NCAR', 'NASA'],
+            'B': ['CESM', 'FOO', 'FOO', 'BAR', 'FOO', 'CESM', 'GCM', 'WACM', 'foo'],
+            'C': ['hist', 'control', 'hist', 'control', 'hist', 'control', 'hist', 'hist', 'HiSt'],
+            'D': ['O2', 'O2', 'O2', 'O2', 'NO2', 'O2', 'O2', 'TA', 'tAs'],
         }
     )
 
