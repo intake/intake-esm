b78c19d57ecda54f4f3e1180c4cd35bdbe83e148
Anderson Banihirwe <axbanihirwe@ualr.edu>
2020-08-10 17:55:20 -0600

    Update tests for source.py

--- tests/test_source.py
+++ tests/test_source.py
@@ -28,28 +28,41 @@ subset_df = search(
     table_id='Amon',
     grid_label='gr',
 )
-group_args = dict(
-    key='foo',
-    df=subset_df,
-    aggregation_dict=aggregation_dict,
-    path_column='path',
-    variable_column='variable_id',
-    data_format='netcdf',
-    format_column=None,
-    cdf_kwargs={'chunks': {'time': 2}},
-)
-
-single_row_args = dict(
-    key='foo',
-    row=df.iloc[0],
-    path_column='path',
-    data_format='netcdf',
-    format_column=None,
-    cdf_kwargs={'chunks': {'time': 2}},
-)
 
 
-def test_esm_group():
+@pytest.fixture(scope='module')
+def group_args():
+    group_args = dict(
+        key='foo',
+        df=subset_df,
+        aggregation_dict=aggregation_dict,
+        path_column='path',
+        variable_column='variable_id',
+        data_format='netcdf',
+        cdf_kwargs={'chunks': {'time': 2}},
+    )
+    return group_args
+
+
+@pytest.fixture(scope='module')
+def single_row_args():
+    single_row_args = dict(
+        key='foo',
+        row=df.iloc[0],
+        path_column='path',
+        data_format='netcdf',
+        cdf_kwargs={'chunks': {'time': 2}},
+    )
+    return single_row_args
+
+
+@pytest.mark.parametrize('df, format_column', [(subset_df, None), (subset_df, 'data_format')])
+def test_esm_group(group_args, df, format_column):
+    if format_column:
+        df[format_column] = 'netcdf'
+        group_args['format_column'] = format_column
+        group_args['data_format'] = None
+        group_args['df'] = df
     source = ESMGroupDataSource(**group_args)
     assert source._ds is None
     ds = source.to_dask()
@@ -61,14 +74,14 @@ def test_esm_group():
     assert source._ds is None
 
 
-def test_esm_group_repr(capsys):
+def test_esm_group_repr(group_args, capsys):
     source = ESMGroupDataSource(**group_args)
     print(repr(source))
     captured = capsys.readouterr()
     assert 'assets:' in captured.out
 
 
-def test_esm_group_ipython_display():
+def test_esm_group_ipython_display(group_args):
     pytest.importorskip('IPython')
     source = ESMGroupDataSource(**group_args)
     with mock.patch('IPython.display.display') as ipy_display:
@@ -93,7 +106,13 @@ def test_esm_group_invalid_df(x):
         _ = ESMGroupDataSource(**args)
 
 
-def test_esm_single_source():
+@pytest.mark.parametrize('row, format_column', [(df.iloc[0], None), (df.iloc[0], 'data_format')])
+def test_esm_single_source(single_row_args, row, format_column):
+    if format_column:
+        row[format_column] = 'netcdf'
+        single_row_args['format_column'] = format_column
+        single_row_args['data_format'] = None
+        single_row_args['row'] = row
     source = ESMDataSource(**single_row_args)
     assert source._ds is None
     ds = source.to_dask()
@@ -104,7 +123,7 @@ def test_esm_single_source():
     assert source._ds is None
 
 
-def test_esm_single_source_repr(capsys):
+def test_esm_single_source_repr(single_row_args, capsys):
     source = ESMDataSource(**single_row_args)
     print(repr(source))
     captured = capsys.readouterr()
