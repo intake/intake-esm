ca3ea2e085ad231f53772bc0e26bab3a3d94cd7d
Anderson Banihirwe <axbanihirwe@ualr.edu>
2020-09-09 07:55:45 -0600

    Add function for printing version information

--- intake_esm/utils.py
+++ intake_esm/utils.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 """ Helper functions for fetching and loading catalog"""
+import importlib
 import json
 import logging
+import sys
 from pathlib import Path
 from urllib.parse import ParseResult, urlparse, urlunparse
 
@@ -104,3 +106,53 @@ def _fetch_catalog(collection_data, esmcol_path):
         return pd.read_csv(catalog_path), catalog_path
 
     return pd.DataFrame(collection_data['catalog_dict']), None
+
+
+def show_versions(file=sys.stdout):  # pragma: no cover
+    """print the versions of intake-esm and its dependencies.
+       Adapted from xarray/util/print_versions.py
+
+    Parameters
+    ----------
+    file : file-like, optional
+        print to the given file-like object. Defaults to sys.stdout.
+    """
+
+    deps = [
+        ('xarray', lambda mod: mod.__version__),
+        ('pandas', lambda mod: mod.__version__),
+        ('intake', lambda mod: mod.__version__),
+        ('intake_esm', lambda mod: mod.__version__),
+        ('fsspec', lambda mod: mod.__version__),
+        ('s3fs', lambda mod: mod.__version__),
+        ('gcsfs', lambda mod: mod.__version__),
+        ('fastprogress', lambda mod: mod.__version__),
+        ('dask', lambda mod: mod.__version__),
+        ('zarr', lambda mod: mod.__version__),
+        ('cftime', lambda mod: mod.__version__),
+        ('netCDF4', lambda mod: mod.__version__),
+        ('requests', lambda mod: mod.__version__),
+    ]
+
+    deps_blob = []
+    for (modname, ver_f) in deps:
+        try:
+            if modname in sys.modules:
+                mod = sys.modules[modname]
+            else:
+                mod = importlib.import_module(modname)
+        except Exception:
+            deps_blob.append((modname, None))
+        else:
+            try:
+                ver = ver_f(mod)
+                deps_blob.append((modname, ver))
+            except Exception:
+                deps_blob.append((modname, 'installed'))
+
+    print('\nINSTALLED VERSIONS', file=file)
+    print('------------------', file=file)
+
+    print('', file=file)
+    for k, stat in sorted(deps_blob):
+        print(f'{k}: {stat}', file=file)
