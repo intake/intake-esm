caa8d9ecf90401f965e94ad2dabbcd93d93356f6
Anderson Banihirwe <axbanihirwe@ualr.edu>
2021-10-12 07:47:51 -0600

    Add tests for search and getitem

--- tests/test_core.py
+++ tests/test_core.py
@@ -5,6 +5,7 @@ import pandas as pd
 import pytest
 
 import intake_esm
+from intake_esm.source import ESMDataSource
 
 from .utils import (
     catalog_dict_records,
@@ -57,3 +58,39 @@ def test_catalog_unique(obj, sep, read_csv_kwargs):
     assert isinstance(uniques, pd.Series)
     assert isinstance(nuniques, pd.Series)
     assert set(uniques.keys()) == set(cat.df.columns)
+
+
+def test_catalog_contains():
+    cat = intake.open_esm_datastore(cdf_col_sample_cesmle)
+    assert 'ocn.20C.pop.h' in cat
+    assert 'ocn.CTRL.pop.h' in cat
+    assert 'ocn.RCP85.pop.h' in cat
+    assert 'foo' not in cat
+
+
+@pytest.mark.parametrize(
+    'path, query, expected_size',
+    [
+        (cdf_col_sample_cesmle, {'experiment': 'CTRL'}, 1),
+        (cdf_col_sample_cesmle, {'experiment': ['CTRL', '20C']}, 2),
+        (cdf_col_sample_cesmle, {}, 0),
+        (cdf_col_sample_cesmle, {'variable': 'SHF', 'time_range': ['200601-210012']}, 1),
+    ],
+)
+def test_catalog_search(path, query, expected_size):
+    cat = intake.open_esm_datastore(path)
+    new_cat = cat.search(**query)
+    assert len(new_cat) == expected_size
+
+
+@pytest.mark.parametrize('key', ['ocn.20C.pop.h', 'ocn.CTRL.pop.h', 'ocn.RCP85.pop.h'])
+def test_catalog_getitem(key):
+    cat = intake.open_esm_datastore(cdf_col_sample_cesmle)
+    entry = cat[key]
+    assert isinstance(entry, ESMDataSource)
+
+
+def test_catalog_getitem_error():
+    cat = intake.open_esm_datastore(cdf_col_sample_cesmle)
+    with pytest.raises(KeyError):
+        cat['foo']
