2c39f9f350134984751ed48e5865aa78587655c7
Tobias Kölling <tobi@die70.de>
2021-11-24 19:51:44 +0100

    Add `to_dask()` method to `esm_datastore` (#403)
    Co-authored-by: Anderson Banihirwe <axbanihirwe@ualr.edu>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- intake_esm/core.py
+++ intake_esm/core.py
@@ -247,6 +247,7 @@ class esm_datastore(Catalog):
         rv = [
             'df',
             'to_dataset_dict',
+            'to_dask',
             'keys',
             'serialize',
             'datasets',
@@ -571,6 +572,32 @@ class esm_datastore(Catalog):
                         raise exc
         self.datasets = self._create_derived_variables(datasets, skip_on_error)
         return self.datasets
+
+    def to_dask(self, **kwargs) -> xr.Dataset:
+        """
+        Convert result to dataset.
+
+        This is only possible if the search returned exactly one result.
+
+        Parameters
+        ----------
+        all parameters are forwarded to :py:func:`~intake_esm.esm_datastore.to_dataset_dict`.
+
+        Returns
+        -------
+        :py:class:`~xarray.Dataset`
+        """
+        if len(self) != 1:  # quick check to fail more quickly if there are many results
+            raise ValueError(
+                f'Expected exactly one dataset. Received {len(self)} datasets. Please refine your search or use `.to_dataset_dict()`.'
+            )
+        res = self.to_dataset_dict(**{**kwargs, 'progressbar': False})
+        if len(res) != 1:  # extra check in case kwargs did modify something
+            raise ValueError(
+                f'Expected exactly one dataset. Received {len(self)} datasets. Please refine your search or use `.to_dataset_dict()`.'
+            )
+        _, ds = res.popitem()
+        return ds
 
     def _create_derived_variables(self, datasets, skip_on_error):
         if len(self.derivedcat) > 0:
