2016640508197694d4618bf60d3b74e2ac66822e
Julia Kent <46687291+jukent@users.noreply.github.com>
2022-03-10 18:03:20 -0700

    attempt at pytest.mark.parametrize

--- tests/test_tutorial.py
+++ tests/test_tutorial.py
@@ -3,23 +3,15 @@ import pytest
 
 import intake_esm
 
-
-@pytest.mark.network
+tutorial_cats = ["aws-cesm2-le", "aws-cmip6", "google-cmip6"]
+@pytest.mark.paramtrize("names", tutorial_cats )
 class TestGetURL:
-    def setUp(self):
-        self.aws_cesm2_le = (
-            'https://raw.githubusercontent.com/intake/intake-esm/main/tutorial-catalogs/AWS-CESM2-LENS.json',
-        )
-        self.aws_cmip6 = (
-            'https://raw.githubusercontent.com/intake/intake-esm/main/tutorial-catalogs/AWS-CMIP6.json',
-        )
-        self.google_cmip6 = 'https://raw.githubusercontent.com/intake/intake-esm/main/tutorial-catalogs/GOOGLE-CMIP6.json'
 
-    def test_get_url(self) -> None:
-        cat_url = intake_esm.tutorial.get_url(self.testfile)
+    def test_get_url(names):
+        cat_url = intake_esm.tutorial.get_url(names)
         assert isinstance(cat_url, str)
 
-    def test_open_from_url(self) -> None:
-        cat_url = intake_esm.tutorial.get_url(self.testfile)
+    def test_open_from_url(names):
+        cat_url = intake_esm.tutorial.get_url(names)
         cat = intake.open_esm_datastore(cat_url)
         assert isinstance(cat, intake_esm.cat.ESMCatalogModel)
