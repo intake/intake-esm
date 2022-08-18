7c68406024e381e3a4f07ec7315adaff45b10787
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-08-18 10:55:26 -0600

    update tutorial module

--- intake_esm/tutorial.py
+++ intake_esm/tutorial.py
@@ -3,19 +3,21 @@ Useful for:
 * users learning intake-esm
 * building tutorials in the documentation.
 """
+from __future__ import annotations
 
-tutorial_catalogs = {
+DEFAULT_CATALOGS = {
     'aws_cesm2_le': 'https://raw.githubusercontent.com/intake/intake-esm/main/tutorial-catalogs/AWS-CESM2-LENS.json',
     'aws_cmip6': 'https://raw.githubusercontent.com/intake/intake-esm/main/tutorial-catalogs/AWS-CMIP6.json',
     'google_cmip6': 'https://raw.githubusercontent.com/intake/intake-esm/main/tutorial-catalogs/GOOGLE-CMIP6.json',
 }
 
 
-def get_url(name, tutorial_catalogs=tutorial_catalogs):
+def get_url(name: str) -> str:
     """
     Get a small-catalog URL from the online repository (requires internet).
     If a local copy is found then always use that to avoid network traffic.
     Available catalogs:
+
     * ``"aws_cesm2_le"``
     * ``"aws_cmip6"``
     * ``"google_cmip6"''
@@ -23,39 +25,31 @@ def get_url(name, tutorial_catalogs=tutorial_catalogs):
     Parameters
     ----------
     name : str
-        Name of the catalog.
-        e.g. 'aws_cmip6'
-    tutorial_catalogs : dict
-        Catalog of tutorial keys mapping to URLs.
+        Name of the catalog. e.g. 'aws_cmip6'
+
+    Returns
+    -------
+    str
+        URL of the catalog.
     """
 
-    dict_check = isinstance(tutorial_catalogs, dict)
+    if name in DEFAULT_CATALOGS:
+        return DEFAULT_CATALOGS[name]
 
-    if dict_check:
-        try:
-            return tutorial_catalogs[name]
-        except KeyError:
-            print(
-                f'KeyError: {name} is an unknown key. Only small-catalogs in our `tutorial-catalogs` directory are supported with this method. List all supported catalog key names with `intake_esm.tutorial.get_available_cats()`.'
-            )
-
-    else:
-        raise TypeError('TypeError: `tutorial_catalogs` must be of type `dict`.')
+    raise KeyError(
+        f'KeyError: {name} is an unknown key. Only small-catalogs in our `tutorial-catalogs`'
+        f'directory are supported with this method. Valid values include: {get_available_cats()} .'
+    )
 
 
-def get_available_cats(tutorial_catalogs=tutorial_catalogs):
+def get_available_cats() -> list[str]:
     """
     Get a list of all supported small-catalog key names that map to URL from the online repository.
 
-    Parameters
-    ----------
-    tutorial_catalogs : dict
-        Catalog of tutorial name keys mapping to URLs.
+    Returns
+    -------
+    list[str]
+        List of all supported small-catalog key names.
     """
-    dict_check = isinstance(tutorial_catalogs, dict)
 
-    if dict_check:
-        return list(tutorial_catalogs.keys())
-
-    else:
-        raise TypeError('TypeError: `tutorial_catalogs` must be of type `dict`.')
+    return list(DEFAULT_CATALOGS.keys())
--- tests/test_tutorial.py
+++ tests/test_tutorial.py
@@ -2,21 +2,10 @@ import intake
 import pytest
 
 import intake_esm
+from intake_esm.tutorial import DEFAULT_CATALOGS
 
-tutorial_cats = [
-    (
-        'aws_cesm2_le',
-        'https://raw.githubusercontent.com/intake/intake-esm/main/tutorial-catalogs/AWS-CESM2-LENS.json',
-    ),
-    (
-        'aws_cmip6',
-        'https://raw.githubusercontent.com/intake/intake-esm/main/tutorial-catalogs/AWS-CMIP6.json',
-    ),
-    (
-        'google_cmip6',
-        'https://raw.githubusercontent.com/intake/intake-esm/main/tutorial-catalogs/GOOGLE-CMIP6.json',
-    ),
-    pytest.param('bad_key', 'bad_url', marks=pytest.mark.xfail),
+tutorial_cats = list(zip(DEFAULT_CATALOGS.keys(), DEFAULT_CATALOGS.values())) + [
+    pytest.param('bad_key', 'bad_url', marks=pytest.mark.xfail)
 ]
 
 
@@ -36,8 +25,6 @@ def test_open_from_url(name, url):
     assert cat == intake.open_esm_datastore(url)
 
 
-@pytest.fixture
-def test_get_available_cats(tutorial_cats):
-    tutorial_cats_dict = dict(tutorial_cats[0:3])
-    cats = intake_esm.tutorial.get_available_cats(tutorial_cats_dict)
-    assert cats == list(tutorial_cats_dict.keys())
+def test_get_available_cats():
+    cats = intake_esm.tutorial.get_available_cats()
+    assert cats == list(DEFAULT_CATALOGS.keys())
