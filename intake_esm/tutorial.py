aaab9d72f815a79da42f9b31fd758f5efe62f9ad
Julia Kent <46687291+jukent@users.noreply.github.com>
2022-08-18 11:02:34 -0600

    add `get_keys()` method to `tutorial.py` (#458)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Anderson Banihirwe <axbanihirwe@ualr.edu>

--- intake_esm/tutorial.py
+++ intake_esm/tutorial.py
@@ -3,19 +3,21 @@ Useful for:
 * users learning intake-esm
 * building tutorials in the documentation.
 """
+from __future__ import annotations
 
-tutorial_catalogs = {
+DEFAULT_CATALOGS = {
     'aws_cesm2_le': 'https://raw.githubusercontent.com/intake/intake-esm/main/tutorial-catalogs/AWS-CESM2-LENS.json',
     'aws_cmip6': 'https://raw.githubusercontent.com/intake/intake-esm/main/tutorial-catalogs/AWS-CMIP6.json',
     'google_cmip6': 'https://raw.githubusercontent.com/intake/intake-esm/main/tutorial-catalogs/GOOGLE-CMIP6.json',
 }
 
 
-def get_url(name, tutorial_catalogs=tutorial_catalogs):
+def get_url(name: str) -> str:
     """
-    Get a small-catalog  URL from the online repository (requires internet).
+    Get a small-catalog URL from the online repository (requires internet).
     If a local copy is found then always use that to avoid network traffic.
-    Available catalog:
+    Available catalogs:
+
     * ``"aws_cesm2_le"``
     * ``"aws_cmip6"``
     * ``"google_cmip6"''
@@ -23,15 +25,31 @@ def get_url(name, tutorial_catalogs=tutorial_catalogs):
     Parameters
     ----------
     name : str
-        Name of the catalog.
-        e.g. 'aws_cmip6'
-    tutorial_catalogs : dict
-        Catalog of tutorial keys mapping to URLs.
+        Name of the catalog. e.g. 'aws_cmip6'
+
+    Returns
+    -------
+    str
+        URL of the catalog.
     """
 
-    try:
-        return tutorial_catalogs[name]
-    except KeyError:
-        print(
-            f'{name} is an unknown key. Only small-catalogs in our `tutorial-catalogs` directory are supported with this method. The supported catalog key names are: "aws_cesm2_le", "aws_cmip6", "google_cmip6".'
-        )
+    if name in DEFAULT_CATALOGS:
+        return DEFAULT_CATALOGS[name]
+
+    raise KeyError(
+        f'KeyError: {name} is an unknown key. Only small-catalogs in our `tutorial-catalogs`'
+        f'directory are supported with this method. Valid values include: {get_available_cats()} .'
+    )
+
+
+def get_available_cats() -> list[str]:
+    """
+    Get a list of all supported small-catalog key names that map to URL from the online repository.
+
+    Returns
+    -------
+    list[str]
+        List of all supported small-catalog key names.
+    """
+
+    return list(DEFAULT_CATALOGS.keys())
--- tests/test_tutorial.py
+++ tests/test_tutorial.py
@@ -2,21 +2,10 @@ import intake
 import pytest
 
 import intake_esm
+from intake_esm.tutorial import DEFAULT_CATALOGS
 
-tutorial_cats = [
-    (
-        'aws_cesm2_le',
-        'https://raw.githubusercontent.com/intake/intake-esm/main/tutorial-catalogs/AWS-CESM2-LENS.json',
-    ),
-    (
-        'aws_cmip6',
-        'https://raw.githubusercontent.com/intake/intake-esm/main/tutorial-catalogs/AWS-CMIP6.json',
-    ),
-    (
-        'google_cmip6',
-        'https://raw.githubusercontent.com/intake/intake-esm/main/tutorial-catalogs/GOOGLE-CMIP6.json',
-    ),
-    pytest.param('bad_key', 'bad_url', marks=pytest.mark.xfail),
+tutorial_cats = list(zip(DEFAULT_CATALOGS.keys(), DEFAULT_CATALOGS.values())) + [
+    pytest.param('bad_key', 'bad_url', marks=pytest.mark.xfail)
 ]
 
 
@@ -34,3 +23,8 @@ def test_open_from_url(name, url):
     cat = intake.open_esm_datastore(cat_url)
     assert isinstance(cat.esmcat, intake_esm.cat.ESMCatalogModel)
     assert cat == intake.open_esm_datastore(url)
+
+
+def test_get_available_cats():
+    cats = intake_esm.tutorial.get_available_cats()
+    assert cats == list(DEFAULT_CATALOGS.keys())
