cc67a332ae8dc926365767154ed49cceb6d0215d
Anderson Banihirwe <axbanihirwe@ualr.edu>
2024-03-01 15:33:10 -0800

    Update .gitignore and MANIFEST.in files, import modules from package directly, and add new files

--- intake_esm/__init__.py
+++ intake_esm/__init__.py
@@ -3,14 +3,11 @@
 """ Top-level module for intake_esm. """
 # Import intake first to avoid circular imports during discovery.
 import intake
-from pkg_resources import DistributionNotFound, get_distribution
 
-from . import tutorial
-from .core import esm_datastore
-from .derived import DerivedVariableRegistry, default_registry
-from .utils import set_options, show_versions
 
-try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:  # pragma: no cover
-    __version__ = '0.0.0'  # pragma: no cover
+from intake_esm import tutorial
+from intake_esm.core import esm_datastore
+from intake_esm.derived import DerivedVariableRegistry, default_registry
+from intake_esm.utils import set_options, show_versions
+
+from intake_esm._version import __version__
